d6659e37dfac76cb4f2181f972cff8fb8c5893ca
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-10-20 04:32:08 +0100

    ..

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,6 +1,39 @@
 <template>
-    <div>
-        Headeer
+    <div class="header_wrapper py-3">
+        <div class="navbar">
+            <div class="logo_wrapper">
+                <img src="@/assets/img/logo.png" width="40"  />
+            </div>
+            <div class=nav_items>
+                <div class="nav_item">
+                    Home 
+                </div>
+                <div class="nav_item">
+                    About Us
+                </div>
+                <div class="nav_item">
+                    Admission
+                </div>
+                <div class="nav_item">
+                    Parents
+                </div>
+                <div class="nav_item">
+                    Academics
+                </div>
+                <div class="nav_item">
+                    Student Life
+                </div>
+                <div class="nav_item">
+                    Blog
+                </div>
+                <div class="nav_item">
+                    Contact Us
+                </div>
+                <div class="nav_item last">
+                    <button class="btn btn-white">Apply Online</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,153 @@
 <template>
-    <MasterLayout>
-        <div>
-            Home Page
-        </div>
-    </MasterLayout>
+   <div>
+       <div class="bg_wrapper">
+            <Header />
+            <div class="banner_wrapper">
+                <div class="row">
+                    <div class="col-md-5">
+                        <h3 class="banner__header__text">High quality education services. better and faster learning solution!</h3>
+                        <p class="banner__header__para mt-3">The phrasal sequence of the Lorem Ipsum text is now so widespread and commonplace t
+                            hat many DTP programmes can generate dummy text using the starting sequence
+                        </p>
+                        <div>
+                            <button class="btn btn-white mt-3">Learn More</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       </div>
+        <section class="boxes_container">
+            <section class="card__boxes__wrapper mt-3">
+            <div class="card-box">
+                <img src="@/assets/img/star (2).svg"  width="40" />
+                <p class="mt-5 green__text bold_text">Nursery</p>
+            </div>
+            <div class="card-box">
+                <img src="@/assets/img/student.svg" width="40" />
+                <p class="mt-5 green__text bold_text">Primary</p>
+            </div>
+            <div class="card-box">
+                <img src="@/assets/img/Outline_7_.svg" width="40" />
+                <p class="mt-5 green__text bold_text">High School</p>
+            </div>
+            <div class="card-box">
+                <img src="@/assets/img/student (1).svg" width="40"  />
+                <p class="mt-5 green__text bold_text">College</p>
+            </div>
+        </section>
+        </section>
+        <section class="upcoming_events_wrapper">
+            <div class="row align-items-center">
+                <div class="col-md-4">
+                    <h3 class="upcoming_events_header">Growing in Knowledge. Following in Faith. Serving in Unity</h3>
+                </div>
+                <div class="col-md-4">
+                    <div class="upcoming_event_founder_card">
+                        <h3 class="upcoming_event_founder_card_para">
+                            At SOL Academy we are commited the excellence in all aspects 
+                            of school life. We encourage children to give of their best in everything they do
+                        </h3>
+                        <div class="row">
+                            <div class="col-md-4 text-right">
+                                <img src="@/assets/img/Eben.jpg" class="img_thumbnail" />
+                            </div>
+                            <div class="col-md-8">
+                                <strong>SQl academy</strong>
+                                <div>
+                                    <small>Founder & CEO, SQL</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="upcoming_events_section">
+                        <h3 class="py-3 text-center">Upcoming Events</h3>
+                        <div class="upcoming_events_section_inner_wrapper">
+                            <div class="list_item mb-2" v-for="(a,i) in 3" :key="i">
+                                <div class="row hundred-percent-height">
+                                    <div class="col-md-4 hundred-percent-height">
+                                        <img src="@/assets/img/write-good-blog-article-get-more-traffic1.png" class="card_img" />
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="row upce_list pt-2" >
+                                            <div class="col-md-7 remove-pading-left">
+                                                <h6 class="blog-title">Blackpool Trip</h6>
+                                            </div>
+                                            <div class="col-md-5 remove-pading-left">
+                                                <span>
+                                                    <i class="fa fa-calendar-o mr-2" aria-hidden="true"></i>
+                                                    <small class="small_text">20 Oct 2020</small>
+                                                </span>
+                                            </div>
+                                            <div class="mt-2">
+                                                <div class="blog_para" >Book unique camping experiences on over 300,000 campsites.</div>
+                                            </div>
+                                            
+                                            <div class="text-center read_more_div">
+                                                <small class="green__text">Read More</small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-center mt-2">
+                                <button class="btn btn-green ">Learn More</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="important_resources_wrapper ">
+            <h3 class="text-center">Important Resources</h3>
+            <div class="row mt-5">
+                <div class="col-md-3 ">
+                    <div class="important_resources_box ">
+                        <div class="circular_div">
+                            <img src="@/assets/img/money.svg"  width="40" />
+                        </div>
+                        <p class="mt-5 green__text bold_text">Fees Structure</p>
+                    </div>
+                </div>
+                <div class="col-md-3 ">
+                    <div class="important_resources_box">
+                        <div class="circular_div">
+                            <img src="@/assets/img/calendar.svg"  width="40" />
+                        </div>
+                        <p class="mt-5 green__text bold_text">Calender</p>
+                    </div>
+                </div>
+                <div class="col-md-3 ">
+                    <div class="important_resources_box">
+                        <div class="circular_div">
+                            <img src="@/assets/img/information.svg"  width="40" />
+                        </div>
+                        <p class="mt-5 green__text bold_text">Application Form</p>
+                    </div>
+                </div>
+                <div class="col-md-3 ">
+                    <div class="important_resources_box">
+                        <div class="circular_div">
+                            <img src="@/assets/img/innovation.svg"  width="40" />
+                        </div>
+                        <p class="mt-5 green__text bold_text">Prospectus</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <Footer />
+   </div>
 </template>
 <script>
-import MasterLayout from '@/views/masterlayout.vue';
+import Header from "./Header"
+import Footer from './Footer';
 
 export default {
     name: 'Home',
     components: {
-        MasterLayout
+        Header,
+        Footer
     }
 }
 </script>
