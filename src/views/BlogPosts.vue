7f3e1d59dedd806f2383d4274d5c0467b3be4604
Omoruyis <omoruyi.isaac97@gmail.com>
2020-10-22 14:15:35 +0100

    updtea

--- src/views/BlogPosts.vue
+++ src/views/BlogPosts.vue
@@ -1,22 +1,55 @@
 <template>
     <MasterLayout>
-        <div class="blog_posts_container">
+        <div class="blog_posts_container content_width">
             <h3 class="section_col_header text-center">Latest Posts</h3>
             <section class="row justify-content-between">
-                <div class="col-md-3">
-                    <img>
-                    <p class="value_name text-center">Character</p>
-                    <p class="value_text text-center">Software development teams used by tools</p>
+                <div class="col-md-4 blog_post">
+                    <img src="@/assets/img/blog-posts-first.png">
+                    <div class="post_header">
+                        <div class="post_date">
+                            <i class="fa fa-calendar-o mr-2" aria-hidden="true"></i>
+                            <p>10 FEB 2020</p>
+                        </div>
+                        <div class="post_date">
+                            <i class="fa fa-user mr-2" aria-hidden="true"></i>
+                            <p>Writer</p>
+                        </div>
+                    </div>
+                    <h4>Blackpool Trip</h4>
+                    <p class="value_name">Book unique camping experiences on over 300,000 campsites.</p>
+                    <router-link>Read More</router-link>
                 </div>
-                <div class="col-md-3">
-                    <img>
-                    <p class="value_name text-center">Leadership</p>
-                    <p class="value_text text-center">Software development teams used by tools</p>
+                <div class="col-md-4 blog_post">
+                    <img src="@/assets/img/blog-posts-first.png">
+                    <div class="post_header">
+                        <div class="post_date">
+                            <i class="fa fa-calendar-o mr-2" aria-hidden="true"></i>
+                            <p>10 FEB 2020</p>
+                        </div>
+                        <div class="post_date">
+                            <i class="fa fa-user mr-2" aria-hidden="true"></i>
+                            <p>Writer</p>
+                        </div>
+                    </div>
+                    <h4>Blackpool Trip</h4>
+                    <p class="value_name">Book unique camping experiences on over 300,000 campsites.</p>
+                    <router-link>Read More</router-link>
                 </div>
-                <div class="col-md-3">
-                    <img>
-                    <p class="value_name text-center">Academics</p>
-                    <p class="value_text text-center">Software development teams used by tools</p>
+                <div class="col-md-4 blog_post">
+                    <img src="@/assets/img/blog-posts-first.png">
+                    <div class="post_header">
+                        <div class="post_date">
+                            <i class="fa fa-calendar-o mr-2" aria-hidden="true"></i>
+                            <p>10 FEB 2020</p>
+                        </div>
+                        <div class="post_date">
+                            <i class="fa fa-user mr-2" aria-hidden="true"></i>
+                            <p>Writer</p>
+                        </div>
+                    </div>
+                    <h4>Blackpool Trip</h4>
+                    <p class="value_name">Book unique camping experiences on over 300,000 campsites.</p>
+                    <router-link>Read More</router-link>
                 </div>
             </section>
         </div>
