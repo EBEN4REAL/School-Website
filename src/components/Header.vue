762fc736bbc7376b66dbe2c26223c624e77586d5
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-11-04 16:34:38 +0100

    ..

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,51 +1,5 @@
 <template>
     <div class="header_wrapper py-3">
-            <!-- <div class=nav_items>
-                <router-link :to="{name: 'home'}" class="text-decoration-none nav_item"  :class="activeTab  === 'home' ?  'activeNav' : null">
-                    <div class="">
-                        Home 
-                    </div>
-                </router-link>
-                <router-link :to="{name: 'about'}" class="text-decoration-none nav_item"  :class="activeTab  === 'about' ?  'activeNav' : null">
-                    <div class="">
-                        About 
-                    </div>
-                </router-link>
-                 <router-link :to="{name: 'about'}" class="text-decoration-none nav_item">
-                    <div class="">
-                        Admission 
-                    </div>
-                </router-link>
-                 <router-link :to="{name: 'about'}" class="text-decoration-none nav_item">
-                    <div class="">
-                        Parents 
-                    </div>
-                </router-link>
-                 <router-link :to="{name: 'about'}" class="text-decoration-none nav_item">
-                    <div class="">
-                        Academics 
-                    </div>
-                </router-link>
-                 <router-link :to="{name: 'about'}" class="text-decoration-none nav_item" >
-                    <div class="">
-                        Student Life 
-                    </div>
-                </router-link>
-                 <router-link :to="{name: 'blogPosts'}" class="text-decoration-none nav_item" :class="activeTab  === 'blogPosts' ?  'activeNav' : null">
-                    <div class="">
-                        Blog 
-                    </div>
-                </router-link>
-                 <router-link :to="{name: 'about'}" class="text-decoration-none nav_item">
-                    <div class="">
-                        contact 
-                    </div>
-                </router-link>
-                <div class="nav_item last">
-                    <button class="school_btn btn-white">Apply Online</button>
-                </div>
-            </div> -->
-    <!-- Fixed navbar -->
     <nav class="navbar navbar-expand-lg navbar-expand-md navbar-light contact-navbar scrolling-navbar px-0 w-100">
          <div class="container">
             <a class="navbar-brand" href="./"><img src="@/assets/img/logo.png" width="40"  />
@@ -119,6 +73,7 @@
                 </router-link>
                           </li>
                           </ul>
+                        
                 <div class="nav_item last">
                     <button class="school_btn btn-white">Apply Online</button>
                 </div>
