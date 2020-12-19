ce86aec910c3a0d37d66669038d00fb3d218f550
Omoruyis <omoruyi.isaac97@gmail.com>
2020-12-19 12:44:25 +0100

    .

--- src/views/Attendance.vue
+++ src/views/Attendance.vue
@@ -2,16 +2,17 @@
     <MasterLayout :title="title" :bg_URL="bg_URL">
         <div class="pace_container content_width uniform_containe">
             <section class="mb-5">
+                <p class="section_col_text para mb-5">We expect students to attend at all times. If a student does not attend school they will not learn. A student's regular attendance and punctuality are a parent's or guardians legal responsibility.</p>
                 <div class="row justify-content-center align-items-center">
-                    <div class="col-xl-6 col-lg-12 mb-3 col-md-12 remove_about_padding">
+                    <div class="col-xl-5 col-lg-12 mb-3 col-md-12 remove_about_padding">
                         <img src="@/assets/img/Attendance.jpg" width="300px" />
                     </div>
-                    <div class="col-xl-6 col-lg-12 col-md-12 codes">
-                        <p class="section_col_text para">We expect students to attend at all times. If a student does not attend school they will not learn. A student's regular attendance and punctuality are a parent's or guardians legal responsibility.
-                        <br>
-                        Recent changes to Government Guidelines regarding acceptable school attendance levels state:
-                        <br>
-                        <i><b>All students must achieve at least 95% attendance as instructed by LEA and any child falling below 95% will now be contacted by the School and meetings will be arranged to discuss how attendance can be improved. All parents of a student who will not be attending school through illness or for other unforeseen reasons are required to telephone the school office before 7:30am to give the reason.</b></i>
+                    <div class="col-xl-7 col-lg-12 col-md-12 codes">
+                        <h3 class="section_col_header">
+                            Recent changes to Government Guidelines regarding acceptable school attendance levels state:
+                        </h3>
+                        <p class="section_col_text para">
+                        All students must achieve at least 95% attendance as instructed by LEA and any child falling below 95% will now be contacted by the School and meetings will be arranged to discuss how attendance can be improved. All parents of a student who will not be attending school through illness or for other unforeseen reasons are required to telephone the school office before 7:30am to give the reason.
                         <br>
                         A note, signed by a parent or guardian, confirming the reason, must be brought after every absence otherwise the absence will be logged as unauthorised. By law, only the School can authorise an absence.
                         <br>
