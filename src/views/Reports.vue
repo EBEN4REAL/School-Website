442bda8bac88bf63f78739e9374b3e113e66ca2d
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2020-12-20 19:40:59 +0100

    ...

--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -11,37 +11,48 @@
             </div>
             <h3 class="section_col_header text-center reports_text">Reports</h3>
             <section class="row justify-content-between reports_holder">
-                <div class="col-md-6 col-lg-6 col-xl-4">
+                <div class="col-md-3">
                     <div :style="[{
                         backgroundImage: `linear-gradient(rgb(38, 40, 49, 0.9), rgb(38, 40, 49, 0.9)), url('https://res.cloudinary.com/dfjzditzc/image/upload/v1572447495/samples/ecommerce/leather-bag-gray.jpg')`,
                         backgroundPosition: 'center',
                         backgroundRepeat: 'no-repeat',
                         backgroundSize: 'cover'
                     }]" class="single_report">
-                        <p class="text-center">OFSTED</p>
-                        <a href="./Ofsted.pdf" download class="text-decoration-none report_link">Download Report</a>
+                        <strong class="text-center text-white">OFSTED</strong>
+                        <a href="./Ofsted.pdf" download class="text-decoration-none report_link mt-3">Download Report</a>
                     </div>
                 </div>
-                <div class="col-md-6 col-lg-6 col-xl-4">
+                <div class="col-md-3 ">
                     <div :style="[{
                         backgroundImage: `linear-gradient(rgb(38, 40, 49, 0.9), rgb(38, 40, 49, 0.85)), url('https://res.cloudinary.com/dfjzditzc/image/upload/v1572447495/samples/ecommerce/leather-bag-gray.jpg')`,
                         backgroundPosition: 'center',
                         backgroundRepeat: 'no-repeat',
                         backgroundSize: 'cover'
                     }]" class="single_report">
-                        <p class="text-center">SOL OFSTED REPORT</p>
-                        <a href="./SOL OFSTED RESULTS.pptx" download class="text-decoration-none report_link">Download Report</a>
+                        <strong class="text-center text-white">SOL OFSTED</strong>
+                        <a href="./SOL OFSTED RESULTS.pptx" download class="text-decoration-none report_link  mt-3">Download Report</a>
                     </div>
                 </div>
-                <div class="col-md-6 col-lg-6 col-xl-4">
+                 <div class="col-md-3 ">
                     <div :style="[{
-                        backgroundImage: `linear-gradient(rgb(38, 40, 49, 0.9), rgb(38, 40, 49, 0.9)), url('https://res.cloudinary.com/dfjzditzc/image/upload/v1572447495/samples/ecommerce/leather-bag-gray.jpg')`,
+                        backgroundImage: `linear-gradient(rgb(38, 40, 49, 0.9), rgb(38, 40, 49, 0.85)), url('https://res.cloudinary.com/dfjzditzc/image/upload/v1572447495/samples/ecommerce/leather-bag-gray.jpg')`,
                         backgroundPosition: 'center',
                         backgroundRepeat: 'no-repeat',
                         backgroundSize: 'cover'
                     }]" class="single_report">
-                        <p>2019</p>
-                        <a href="./SOL OFSTED RESULTS.pptx" download class="text-decoration-none report_link">Download Report</a>
+                        <strong class="text-center text-white">SOL OFSTED</strong>
+                        <a href="./SOL OFSTED RESULTS.pptx" download class="text-decoration-none report_link  mt-3">Download Report</a>
+                    </div>
+                </div>
+                 <div class="col-md-3 ">
+                    <div :style="[{
+                        backgroundImage: `linear-gradient(rgb(38, 40, 49, 0.9), rgb(38, 40, 49, 0.85)), url('https://res.cloudinary.com/dfjzditzc/image/upload/v1572447495/samples/ecommerce/leather-bag-gray.jpg')`,
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundSize: 'cover'
+                    }]" class="single_report">
+                        <strong class="text-center text-white">SOL OFSTED</strong>
+                        <a href="./SOL OFSTED RESULTS.pptx" download class="text-decoration-none report_link  mt-3">Download Report</a>
                     </div>
                 </div>
                 
